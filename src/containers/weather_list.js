b4bf305e8b658a365fd7fa80f85711a4a7f1e74b
Alexandre PONCET <alexandreponcet.mail@gmail.com>
2016-11-21 23:58:42 +0100

    indentation plus

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -3,7 +3,6 @@ import { connect } from 'react-redux';
 import Chart from '../components/chart';
 import GoogleMap from '../components/google_map';
 
-
 class WeatherList extends Component {
   renderWeather(cityData) {
     const name = cityData.city.name;
