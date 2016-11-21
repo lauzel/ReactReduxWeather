de70cb299f1aacf8f0a875302be8f52f636dfd3d
Alexandre PONCET <alexandreponcet.mail@gmail.com>
2016-11-21 21:18:34 +0100

    Add onInputChange callback on Component SearchBar

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,13 +1,30 @@
 import React, {Component} from 'react';
 
-
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: '' };
+
+    //Bind this to SearchBar this
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  onInputChange(event) {
+    this.setState({ term: event.target.value  });
+  }
+
   render() {
     return (
       <form className="input-group">
-          <input />
+          <input
+            placeholder="Récuperer le temps sur 5 jours de votre ville"
+            className="form-control"
+            value={this.state.term}
+            onChange={this.onInputChange}
+            />
           <span className="input-group-btn">
-            <button type="submit" className="btn btn-secondary">Submit</button>
+            <button type="submit" className="btn btn-secondary">Recherche</button>
           </span>
       </form>
     );
